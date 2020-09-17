801646d2cc5f6573b25f989edfb27c1262088d4b
Andreas <Fyziikme@gmail.com>
2020-09-17 09:04:55 +0200

    first init

---
+++ src/main/java/dk/fyziik/FyziikApplication.java
@@ -0,0 +1,13 @@
+package dk.fyziik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyziikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FyziikApplication.class, args);
+    }
+
+}
