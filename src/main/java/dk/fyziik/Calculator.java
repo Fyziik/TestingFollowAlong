dc7ab066c20c4b77bfb450146a629efb3e82c2f7
Andreas <Fyziikme@gmail.com>
2020-09-17 09:09:12 +0200

    first commit

---
+++ src/main/java/dk/fyziik/Calculator.java
@@ -0,0 +1,11 @@
+package dk.fyziik;
+
+public class Calculator {
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
+
---
+++ src/main/java/dk/fyziik/Main.java
@@ -0,0 +1,10 @@
+package dk.fyziik;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1, 2));
+    }
+}
