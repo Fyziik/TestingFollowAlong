3beb885814f2344fea67fdacced8c5cfd29348cb
Andreas <Fyziikme@gmail.com>
2020-09-17 11:05:32 +0200

    finished Collections Exercises Tests

--- src/test/java/dk/fyziik/CollectionsExercisesTest.java
+++ src/test/java/dk/fyziik/CollectionsExercisesTest.java
@@ -3,19 +3,49 @@ package dk.fyziik;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollectionsExercisesTest {
 
+    CollectionsExercises ce;
+
     @BeforeEach
     void setUp() {
+        ce = new CollectionsExercises();
     }
 
     @Test
     void matchEnds() {
+        //    words = ['aba', 'xyz', 'aa', 'x', 'bbb'] --> returns 3
+        ArrayList<String> tmp = new ArrayList<>();
+        tmp.add("aba");
+        tmp.add("xyz");
+        tmp.add("aa");
+        tmp.add("x");
+        tmp.add("bbb");
+        assertEquals(3, ce.matchEnds(tmp));
     }
 
     @Test
     void frontX() {
+        //        e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+        //        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+        ArrayList<String> toBeSorted = new ArrayList<>();
+        toBeSorted.add("mix");
+        toBeSorted.add("xyz");
+        toBeSorted.add("apple");
+        toBeSorted.add("xanadu");
+        toBeSorted.add("aardvark");
+
+        ArrayList<String> sorted = new ArrayList<>();
+        sorted.add("xanadu");
+        sorted.add("xyz");
+        sorted.add("aardvark");
+        sorted.add("apple");
+        sorted.add("mix");
+
+        assertEquals(sorted, ce.frontX(toBeSorted));
     }
 }
